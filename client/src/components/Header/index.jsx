be3fdc86529e47ed89e375ce26948125d18ce7a3
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-09-04 13:03:31 -0700

    building navbar

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 const Header = () => {
   return (
-    <header>
-      <div className="container text-center">
-        This is header with a link 
-        <Link to="/">
-          <h1>
-            Home
-          </h1>
-        </Link>
-      </div>
-    </header>
+    <Navbar>
+      <Container className="container text-center">
+        <Navbar.Brand>PropertyPulse</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="navbar">
+            <Nav.Link href="/rent">Rent</Nav.Link>
+            <Nav.Link href="/buy">Buy</Nav.Link>
+            <Nav.Link href="/forum">Forum</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/signup">Signup</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    
+      </Container>
+    </Navbar>
   );
 };
 
