784b1074cd7e73b9b41f4e50661a2e5f16009128
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-09-04 11:33:31 -0400

    added signup.jsx page

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,15 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 // mutations will be here
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
