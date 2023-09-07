38ba35d8fdda0097c552cffa29c3ffaafd3d6d73
vlada_caban <vladaonly@gmail.com>
2023-09-06 20:31:14 -0400

    added resolvers for forum

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,20 @@ const { User, ForumTopic, ForumComment } = require("../models");
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
-  // resolvers will be here
+  Query: {
+    //User query resolvers
+
+    //Forum query resolvers
+    getAllForumTopics: async () => {
+      return ForumTopic.find();
+    },
+
+    getOneForumTopic: async (parent, { topicId }) => {
+      return ForumTopic.findOne({ _id: topicId });
+    },
+
+  
+  },
 };
 
 module.exports = resolvers;
