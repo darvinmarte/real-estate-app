31b1041c8b78d30b1986416b23d154ced644931e
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-09-07 18:24:38 -0700

    removed navbar color

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 10000); // Change image every 5 seconds
+    }, 5000); // Change image every 5 seconds
 
     return () => clearInterval(interval);
   }, []);
