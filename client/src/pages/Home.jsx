4342d7cb1ff8a4eee96ffea8572777972c7b1115
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-09-07 18:45:11 -0700

    i don't think there are any changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import "./pages.css"
 
 //images provided from unsplash.com
@@ -19,10 +19,10 @@ const Home = () => {
 
   return (
     <main>
-      {/* <div className="justify-center"> */}
+      <div className="justify-center">
         <img src={images[currentIndex]} alt={`Image ${currentIndex + 1}`} className ="hero-image"/>
         <div className="text">{texts[currentIndex]}</div>
-      {/* </div> */}
+      </div>
     </main>
   );
 };
